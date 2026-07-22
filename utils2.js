14dbd953bc1b137ba291119106426a022a6c3e0b
STDHenry <anhkhoa0727@gmail.com>
2026-07-22 19:28:15 +0700

    Add files via upload

--- utils2.js
+++ utils2.js
@@ -2,7 +2,7 @@
 // INDEPENDENT CITY NETWORK - KHO 2 (MAIN APP) - UTILS2.JS (ĐỒNG BỘ MINI CHAT)
 // =================================================================
 
-const SUPABASE_URL = "https://accvbywalbbkowbwuspd.supabase.co";
+const SUPABASE_URL = "https://lzvtnkpnsulawxxigcpd.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx6dnRua3Buc3VsYXd4eGlnY3BkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyOTA2NjksImV4cCI6MjA5OTg2NjY2OX0.rPsGAcSt3Yv049dxUTVNaUlpw8hHEpTpr84HYv7dHEg";
 
 let cachedSupabaseClient = null;
